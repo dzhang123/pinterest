a58efb262dd6028df29d46ca29f8052bbf1679b1
Dongming Zhang <dzhang123@gmail.com>
2019-02-04 21:36:39 -0600

    dotnet

---
+++ dotnet/CSharpTutorial/CSharpTutorial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static void Main(string[] args)
+        {
+            ProcessRepositories().Wait();
+             Console.WriteLine("done last");
+        }
+
+        private static async Task ProcessRepositories () {
+            var serializer = new DataContractJsonSerializer(typeof(List<repo>));
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            //var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            //var msg = await stringTask;
+            // Console.write(msg);
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<repo>;
+
+
+
+        }
+    }
+}
